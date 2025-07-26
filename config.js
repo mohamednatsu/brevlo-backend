e1b1bd3e539032a978f8be7f0ed21733a1504cdd
mohamednatsu <msalhnwr@gmail.com>
2025-07-26 18:35:15 +0300

    add updates in summary and auth to use prisma

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-       ASSEMBLYAI_API_KEY: "138c0e246e0d41919d97e4b7ae8608a0",
-       GROQ_API_KEY: "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",
+       ASSEMBLYAI_API_KEY: process.env.assemplyai_key,
+       GROQ_API_KEY: process.env.GROQ_API_KEY,
        session: {
               secret: process.env.SESSION_SECRET || 'your-secret-key',
               maxAge: parseInt(process.env.SESSION_MAX_AGE) || 24 * 60 * 60 * 1000, // 24 hours
